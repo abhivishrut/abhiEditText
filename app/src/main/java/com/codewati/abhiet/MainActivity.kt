072148efb74e604061e6654b4d29e2be1a65ca21
Abhivishrut Bishnoi <80168406+abhivishrut@users.noreply.github.com>
2025-03-19 17:24:09 +0530

    update 1.0.1

--- app/src/main/java/com/codewati/abhiet/MainActivity.kt
+++ app/src/main/java/com/codewati/abhiet/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.codewati.abhiet
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.codewati.abhiedittext.abhiEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var customInput: abhiEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        customInput = findViewById(R.id.customInput)
+
+        val inputText = customInput.getText()
+        Toast.makeText(this, "Entered: $inputText", Toast.LENGTH_SHORT).show()
+
+
     }
 }
